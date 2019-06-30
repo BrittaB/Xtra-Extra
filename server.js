49f3e2458badbcc5a1a59c174d3dc15ef1130b4f
Britta <polished.athena@gmail.com>
2019-06-30 16:48:59 -0500

    working on server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+//var requires
 var express = require ("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
 var axios = require ("axios");
+
+//var cheerio
 var cheerio = ("cheerio");
 var db = ("./models");
+
+//port express
+var PORT = 3000;
+var app = express ();
