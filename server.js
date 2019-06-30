8bdb64383791638f2407e336c9a2d5c1b7fd979c
Britta <polished.athena@gmail.com>
2019-06-30 16:44:47 -0500

    updated files, started on server

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-var cheerio = require("cheerio");
-var axios = require("axios");
+var express = require ("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-console.log
-("\n****************\n"+
- "grabbing every article and linking\n"+ "from nytimes main page:" + "\n********\n");
-axios.get("https://www.nytimes.com").then(function(response) {
-
-  var $ = cheerio.load(response.data);
-  var results = [];
-  $("article").each(function(i, element) {
-
-    var title = $(element).children().text();
-    var link = $(element).find("a").attr("href");
-
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  console.log(results);
-});
+var axios = require ("axios");
+var cheerio = ("cheerio");
+var db = ("./models");
